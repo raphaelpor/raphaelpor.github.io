fec5cff8b60e56ce03fd99e4c8425894df595bb0
Raphael Porto <raphaelpor@gmail.com>
2016-10-16 01:44:33 -0200

    Add fixed size for Avatar

--- src/shared/Avatar/index.js
+++ src/shared/Avatar/index.js
@@ -8,6 +8,8 @@ function Avatar() {
         src="https://dummyimage.com/100x100/000/fff.png"
         alt="Site name"
         title="Site name"
+        width="100"
+        height="100"
       />
     </div>
   );
