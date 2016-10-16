c9dbab47ec76b182256c535bd3181fd15191e073
Raphael Porto <raphaelpor@gmail.com>
2016-10-16 02:30:49 -0200

    Add social links on data.js

--- src/services/data.js
+++ src/services/data.js
@@ -2,4 +2,22 @@ export default {
   name: 'Raphael Porto',
   role: 'Front End Developer',
   company: 'Concrete Solutions',
+  links: [
+    {
+      name: 'Twitter',
+      url: 'https://',
+    },
+    {
+      name: 'GirHub',
+      url: 'https://',
+    },
+    {
+      name: 'Linkedin',
+      url: 'https://',
+    },
+    {
+      name: 'CV',
+      url: 'https://',
+    },
+  ],
 };
