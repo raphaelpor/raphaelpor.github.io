9c7502929365cfb6c3e5d880cf4da4675d95b78d
Raphael Porto <raphaelpor@gmail.com>
2016-10-15 21:09:00 -0300

    Add Intro to Home

--- src/views/Home/Home.spec.js
+++ src/views/Home/Home.spec.js
@@ -7,15 +7,6 @@ import Home from './';
 describe('<Home />', function() {
   const wrapper = shallow(<Home />);
 
-  it('has a <h1>', function() {
-    expect(wrapper.html()).to.contain('<h1');
-    expect(wrapper.html()).to.contain('</h1>');
-  });
-
-  it('shows "Hello!"', function() {
-    expect(wrapper.text()).to.contain("Hello!");
-  });
-
   it('has a <div>', function() {
     expect(wrapper.html()).to.contain('<div>');
     expect(wrapper.html()).to.contain('</div>');
--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import Avatar from '../../shared/Avatar';
+import Intro from './components/Intro';
 
 function Home() {
   return (
     <div>
       <Avatar />
-      <h1>Hello!</h1>
+      <Intro />
     </div>
   );
 }
