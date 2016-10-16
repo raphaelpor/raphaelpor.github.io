f6698909febf7e07ba8789e730cdc715385ece10
Raphael Porto <raphaelpor@gmail.com>
2016-10-15 21:08:41 -0300

    Add <Intro />

---
+++ src/views/Home/components/Intro/Intro.spec.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import Intro from './';
+
+describe('<Intro />', function() {
+  const wrapper = shallow(<Intro />);
+
+  it('has a <div>', function() {
+    expect(wrapper.html()).to.contain('<div>');
+    expect(wrapper.html()).to.contain('</div>');
+  });
+
+  it('has a <h1>', function() {
+    expect(wrapper.html()).to.contain('<h1');
+    expect(wrapper.html()).to.contain('</h1>');
+  });
+
+  it('has a <small>', function() {
+    expect(wrapper.html()).to.contain('<small');
+    expect(wrapper.html()).to.contain('</small>');
+  });
+
+  it('shows "test!"', function() {
+    expect(wrapper.text()).to.contain("test");
+  });
+
+});
---
+++ src/views/Home/components/Intro/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Intro() {
+  return (
+    <div>
+      <h1>test</h1>
+      <small>test</small>
+    </div>
+  );
+}
+
+export default Intro;
