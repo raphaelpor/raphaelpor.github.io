f8826c4fdb64adcca7ed0ccd45d35ff0a4aeae56
Raphael Porto <raphaelpor@gmail.com>
2016-10-25 21:51:10 -0200

    Add CoreSkills on Skills

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import './style.css';
 
+import CoreSkills from './components/CoreSkills';
+
 function Skills() {
-  return <div className="Skills container" />;
+  return (
+    <div className="Skills container">
+      <CoreSkills />
+    </div>
+  );
 }
 
 export default Skills;
