c69aec64f78295807b094adca88c164d6929747a
Raphael Porto <raphaelpor@gmail.com>
2016-10-25 22:03:43 -0200

    Add id and title to Skillns

--- src/views/Skills/index.js
+++ src/views/Skills/index.js
@@ -5,7 +5,8 @@ import CoreSkills from './components/CoreSkills';
 
 function Skills() {
   return (
-    <div className="Skills container">
+    <div id="skills" className="Skills container">
+      <h1>Core Skills</h1>
       <CoreSkills />
     </div>
   );
